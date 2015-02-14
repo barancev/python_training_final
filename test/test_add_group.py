f893e8d3caed5dcd1b598d26ee0cf0600263c6f5
Alexei Barantsev <barancev@gmail.com>
2015-02-14 19:49:13 +0300

    Созданы пакеты для разных зон ответственности

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+from model.group import Group
+from fixture.application import Application
 
 
 @pytest.fixture
