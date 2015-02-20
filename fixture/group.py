c745bec251ac5bfcf441e5b36c15f40cd9cc09ee
Alexei Barantsev <barancev@gmail.com>
2015-02-20 16:24:03 +0300

    Выполнена оптимизация переходов на страницу со списком групп

--- fixture/group.py
+++ fixture/group.py
@@ -8,7 +8,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
