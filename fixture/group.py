33aa8caa48eee3193a6bd83bc1b18dc2f9620367
Alexei Barantsev <barancev@gmail.com>
2015-02-20 15:15:35 +0300

    Реализована проверка предусловия в тестах для удаления групп

--- fixture/group.py
+++ fixture/group.py
@@ -61,3 +61,7 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
 
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,6 +1,9 @@
 __author__ = 'alexei'
+from model.group import Group
 
 
 def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.delete_first_group()
 
