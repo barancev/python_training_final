8b6c32fe8899cd24e9e4ab5c3954f91434aa9690
Alexei Barantsev <barancev@gmail.com>
2015-03-09 17:50:27 +0300

    Реализовано сравнение списков в тесте для удаления группы

--- model/group.py
+++ model/group.py
@@ -8,3 +8,9 @@ class Group:
         self.header = header
         self.footer = footer
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -9,4 +9,5 @@ def test_delete_first_group(app):
     app.group.delete_first_group()
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
-
+    old_groups[0:1] = []
+    assert old_groups == new_groups
