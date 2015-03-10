373c5ba55c0f765b2ca7644fd12f142f65e4cef9
Alexei Barantsev <barancev@gmail.com>
2015-03-10 16:18:32 +0300

    Реализовано хеширование для списка групп в тесте для добавления группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,18 +5,18 @@ def test_add_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="dfgdfg", header="dfgdfg", footer="dfgfghgfhg")
     app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+#def test_add_empty_group(app):
+#    old_groups = app.group.get_group_list()
+#    group = Group(name="", header="", footer="")
+#    app.group.create(group)
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) + 1 == len(new_groups)
+#    old_groups.append(group)
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
