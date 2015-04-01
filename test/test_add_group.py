b454c5f24139b2eb55e599f91339c520aa4563a2
Alexei Barantsev <barancev@gmail.com>
2015-04-01 12:19:53 +0300

    Тестовые данные отделены от сценария

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+from data.add_group import constant as testdata
 
 
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(5)
-]
+
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
