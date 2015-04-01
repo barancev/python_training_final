745770ab8a6116eaf4b58db0d8af113de3e8bf63
Alexei Barantsev <barancev@gmail.com>
2015-04-01 11:56:21 +0300

    Устранено дублирование имен переменных

--- conftest.py
+++ conftest.py
@@ -15,8 +15,8 @@ def app(request):
     browser = request.config.getoption("--browser")
     if target is None:
         config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
-        with open(config_file) as config_file:
-            target = json.load(config_file)
+        with open(config_file) as f:
+            target = json.load(f)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=target['baseUrl'])
     fixture.session.ensure_login(username=target['username'], password=target['password'])
