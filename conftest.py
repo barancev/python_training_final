2c9ca5cda0aa4d23a6c48cc478a73a886916ad39
Alexei Barantsev <barancev@gmail.com>
2015-04-07 15:23:28 +0300

    Реализованы отключаемые проверки UI

--- conftest.py
+++ conftest.py
@@ -51,9 +51,15 @@ def stop(request):
     return fixture
 
 
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
+
+
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
 
 
 def pytest_generate_tests(metafunc):
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,7 +3,7 @@ from model.group import Group
 import random
 
 
-def test_delete_some_group(app, db):
+def test_delete_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     old_groups = db.get_group_list()
@@ -13,3 +13,5 @@ def test_delete_some_group(app, db):
     assert len(old_groups) - 1 == len(new_groups)
     old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
