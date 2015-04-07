99aa27e51e0554b56a39c0555faec575261c5795
Alexei Barantsev <barancev@gmail.com>
2015-04-07 17:31:19 +0300

    Реализован метод для извлечения контактов из БД

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,12 +1,12 @@
 __author__ = 'alexei'
-import mysql.connector
+from fixture.db import DbFixture
 
-connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
+    contacts = db.get_contact_list()
+    for contact in contacts:
+        print(contact)
+    print(len(contacts))
 finally:
-    connection.close()
+    db.destroy()
--- fixture/db.py
+++ fixture/db.py
@@ -1,6 +1,7 @@
 __author__ = 'alexei'
 import mysql.connector
 from model.group import Group
+from model.contact import Contact
 
 
 class DbFixture:
@@ -21,6 +22,18 @@ class DbFixture:
             for row in cursor:
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
         finally:
             cursor.close()
         return list
